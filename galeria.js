f4ae8db737733a74cab0c42778c8e55e204c8dd3
Eduardo-R-Machado <eduardormachado20@gmail.com>
2022-12-29 13:44:47 -0300

    Conectado firebase storage

--- galeria.js
+++ galeria.js
@@ -6,16 +6,18 @@ function showImageFirebaseStorage() {
      .child('images')
      .listAll()
      .then(function (result) {
-      result.items.map(function (imageRef) {
-       imageRef.getDownloadURL().then(function (url) {
-        var img = document.createElement('img');
-        img
-       });
-      });
+        result.items.map(function (imageRef) {
+            imageRef.getDownloadURL().then(function (url) {
+            let cardItem = document.querySelector('.card').cloneNode(true);
+            document.querySelector('.wrp-cards').appendChild(cardItem);
+            cardItem.querySelector('.card-img').src = url;
+            cardItem.querySelector('.card-name').innerHTML = imageRef.name;
+     })
 
-     }).catch(function (error) {
-  console.log(error);
- });
+})     
+})
+
 }
+
 
 showImageFirebaseStorage();
